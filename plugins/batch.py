b44cf974f87e886e01aa85b6dfb1fd6e99b8150a
RC <136341945+rcgit0@users.noreply.github.com>
2025-06-11 15:50:37 +0530

    Update batch.py

--- plugins/batch.py
+++ plugins/batch.py
@@ -288,10 +288,20 @@ async def process_msg(c, u, m, d, lt, uid, i):
                     mtd = await get_video_metadata(f)
                     dur, h, w = mtd['duration'], mtd['width'], mtd['height']
                     th = await screenshot(f, dur, d)
-                    await c.send_video(tcid, video=f, caption=ft if m.caption else None, 
-                                    thumb=th, width=w, height=h, duration=dur, 
-                                    progress=prog, progress_args=(c, d, p.id, st), 
-                                    reply_to_message_id=rtmid)
+thumb_arg = {"thumb": th} if th and os.path.exists(th) else {}
+
+await c.send_video(
+    tcid,
+    video=f,
+    caption=ft if m.caption else None,
+    width=w,
+    height=h,
+    duration=dur,
+    progress=prog,
+    progress_args=(c, d, p.id, st),
+    reply_to_message_id=rtmid,
+    **thumb_arg  # only included if valid
+)
                 elif m.video_note:
                     await c.send_video_note(tcid, video_note=f, progress=prog, 
                                         progress_args=(c, d, p.id, st), reply_to_message_id=rtmid)
